7b45c04093b900c6af5cb812b753c8a751c6025f
hoaaminzy <129186660+hoaaminzy@users.noreply.github.com>
2025-03-30 21:45:17 +0700

    a

--- server.js
+++ server.js
@@ -216,9 +216,9 @@ app.get("/", (req, res) => {
 });
 // Khởi chạy server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server chạy tại http://localhost:${PORT}`);
-});
+// server.listen(PORT, "0.0.0.0", () => {
+//   console.log(`Server chạy tại http://localhost:${PORT}`);
+// });
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server chạy tại http://localhost:${PORT}`);
