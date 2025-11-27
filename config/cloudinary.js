01ed38b367b90580c74de4888408637ba1489647
LeTanHoa <96413637+LeTanHoa@users.noreply.github.com>
2025-11-27 21:56:02 +0700

    a1

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,9 +2,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: dsnu89olo,
-  api_key: 299146623823736,
-  api_secret: RY_JZwAro-5tcob6IlL7PBlp_nw
+  cloud_name: "dsnu89olo",
+  api_key: "299146623823736",
+  api_secret: "RY_JZwAro-5tcob6IlL7PBlp_nw"
 });
 
 module.exports = cloudinary;
